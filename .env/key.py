508a305086ccf8a384001782ee957e265d11d2e9
Lê Hoàng Anh Duy <163821243+duyviqro1@users.noreply.github.com>
2026-07-03 17:44:33 +0700

    Delete .env directory

--- .env/key.py
+++
@@ -1,7 +0,0 @@
-# Configuration Keys
-OPENAI_API_KEY = "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"
-TAVILY_API_KEY = "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"
-QDRANT_LOCAL_URL = "http://qdrant:6333"
-QDRANT_LOCAL_API_KEY = "qdrant_api_key"
-QDRANT_LOCAL_COLLECTION = "medical_docs"
-LOCAL_DATABASE_URL = "postgresql://admin:admin@postgres_shared:5432/rag_lakehouse"
